260624e3a6ea9d1b99c0683282a011f5d6326c71
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2018-07-01 16:54:10 -0300

    La configuration initiale

---
+++ WebApp01/WebApp01/Models/WebApp01Context.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace WebApp01.Models
+{
+    public class WebApp01Context : DbContext
+    {
+        public WebApp01Context() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
