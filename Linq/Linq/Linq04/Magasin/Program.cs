38912288cad9357d246c8b59063d4675b1facdcf
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2018-07-04 00:12:26 -0300

    L'exemple Linq_04

--- Linq/Linq/Linq04/Magasin/Program.cs
+++ Linq/Linq/Linq04/Magasin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Magasin.Domaine.Entites;
+using Newtonsoft.Json;
 using System;
 using System.Linq;
 
@@ -10,14 +11,34 @@ namespace Magasin
         static void Main(string[] args)
         {
 
-            var prod = new Produit();
+            var prod = new Produit().Lister();
+
             
             // where return un Inumerable
-            prod.Lister().Where(x => x.Valeur < 10 && x.Quantite > 5).ToList().ForEach(p =>
+            prod.Where(x => x.Valeur < 10 && x.Quantite > 5).ToList().ForEach(p =>
               {
-                  Console.WriteLine("Produit: " + p.Description + " Valeur: " + p.Valeur +
-                      " Expiration: " + p.Expiration);
+
+
+                   Console.WriteLine("Produit: " + p.Description + " Valeur: " + p.Valeur +
+                       " Expiration: " + p.Expiration);
+
               });
+
+
+            // JSON FORMAT
+            prod = prod.Where(x => x.Valeur < 10 && x.Quantite > 5 && x.Description.ToUpper().Contains("A") ).ToList();
+
+           
+           prod.ForEach(p =>
+            {
+
+                Console.WriteLine("");
+                Console.WriteLine("--------JSON FORMAT-------------");
+                Console.WriteLine(JsonConvert.SerializeObject(p));
+
+            });
+
+
 
 
             Console.ReadKey();
