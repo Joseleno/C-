60fdd94ea8336d2d6c405009aa75ab8839118e5d
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2018-07-06 21:35:20 -0300

    nouveaux exemples

---
+++ Linq/Linq/Linq04/Magasin/ProduitSelected.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magasin
+{
+    class ProduitSelected
+    {
+        public string Description { get; set; }
+
+        public decimal Valeur { get; set; }
+
+        public DateTime Expiration { get; set; }
+    }
+}
--- Linq/Linq/Linq04/Magasin/Program.cs
+++ Linq/Linq/Linq04/Magasin/Program.cs
@@ -12,8 +12,8 @@ namespace Magasin
         {
 
             var prod = new Produit().Lister();
+            var prod2 = new Produit().Lister(); 
 
-            
             // where return un Inumerable
             prod.Where(x => x.Valeur < 10 && x.Quantite > 5).ToList().ForEach(p =>
               {
@@ -38,10 +38,56 @@ namespace Magasin
 
             });
 
+            Console.WriteLine("");
+            Console.WriteLine("--------Exemple 3-------------");
+            // Un autre exemple
+            var descritions = prod2
+                .Where(p => p.Description.ToUpper().StartsWith("P"))
+                .Select(p => p.Description)
+                .ToList();
+
+            descritions.ForEach(X =>
+            {
+                Console.WriteLine(X);
+            });
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("--------Exemple 4-------------");
+            // exemple 4 generic objet
+            var objets = prod2
+                .Where(p => p.Description.ToUpper().StartsWith("P"))
+                .Select(p => new { p.Description, p.Valeur, p.Expiration })
+                .ToList();
+
+            objets.ForEach(X =>
+            {
+                Console.WriteLine(X);
+            });
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("--------Exemple 5-------------");
+            // exemple 5 Typed Objet
+            var resultSelected = prod2
+                .Where(a => a.Description.ToUpper().StartsWith("P"))
+                .Select(a => new ProduitSelected { Description = a.Description, Valeur = a.Valeur, Expiration = a.Expiration })
+                .ToList();
+
+            resultSelected.ForEach(X =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject( X));
+            });
+
+
+
 
 
 
             Console.ReadKey();
+
 
         }
 
