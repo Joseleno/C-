d75b6ba929cde92189d24d894533ce332932b16c
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2018-07-10 19:54:43 -0300

    Seulement Linq

---
+++ Linq/Linq02/LinqApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Magasin.Infra.Linq.Repositorie;
+using System;
+
+
+namespace LinqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new RepoCategorie().AddCategorie(1, "Électroniques");
+            //new RepoCategorie().AddCategorie(2, "Boissons");
+            //new RepoCategorie().AddCategorie(3, "Fruits");
+            new RepoCategorie().AddCategorie(4, "vêtements"); 
+
+            new RepoCategorie().ModifierCategorie(4, "Vêtements");
+            new RepoCategorie().SupprimerCategorie(4);
+
+        }
+    }
+}
---
+++ Linq/Linq02/Magasin.Infra.Linq/Repositorie/RepoCategorie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magasin.Infra.Linq.Repositorie
+{
+    public class RepoCategorie
+    {
+        public void AddCategorie(int id, string nom)
+        {
+            Categorie cat = new Categorie();
+            cat.Id = id;
+            cat.Description = nom;
+
+            MagasinDataContext linq = new MagasinDataContext();
+            linq.Categorie.InsertOnSubmit(cat);
+
+            linq.SubmitChanges();
+        }
+
+        public void ModifierCategorie(int id, string nom)
+        {
+            MagasinDataContext linq = new MagasinDataContext();
+
+            var cat = linq.Categorie.First(x => x.Id == id);
+
+            cat.Description = nom;
+
+            
+            linq.SubmitChanges();
+        }
+
+
+        public void SupprimerCategorie(int id)
+        {
+            MagasinDataContext linq = new MagasinDataContext();
+
+            var cat = linq.Categorie.First(x => x.Id == id);
+
+            linq.Categorie.DeleteOnSubmit(cat);
+
+            linq.SubmitChanges();
+        }
+        
+    }
+}
