2042c17eed3987932f5afd7dc5751ce8ba930a58
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2018-07-15 19:37:58 -0300

    L'exemple dans un base de données

--- Linq/Linq02/LinqApp/Program.cs
+++ Linq/Linq02/LinqApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Magasin.Infra.Linq.Repositorie;
+using Newtonsoft.Json;
 using System;
 
 
@@ -11,10 +12,28 @@ namespace LinqApp
             //new RepoCategorie().AddCategorie(1, "Électroniques");
             //new RepoCategorie().AddCategorie(2, "Boissons");
             //new RepoCategorie().AddCategorie(3, "Fruits");
-            new RepoCategorie().AddCategorie(4, "vêtements"); 
+            //new RepoCategorie().AddCategorie(4, "vêtements"); 
 
-            new RepoCategorie().ModifierCategorie(4, "Vêtements");
-            new RepoCategorie().SupprimerCategorie(4);
+            //new RepoCategorie().ModifierCategorie(4, "Vêtements");
+            //new RepoCategorie().SupprimerCategorie(4);
+
+            //new RepoProduit().AddProduit(1, 1, "Ordinateur ACER", 850);
+            //new RepoProduit().AddProduit(2, 1, "Ordinateur SONNY", 1050);
+            //new RepoProduit().AddProduit(3, 1, "Ordinateur DELL", 950);
+            //new RepoProduit().AddProduit(4, 1, "Ordinateur SAMSUNG", 1150);
+            //new RepoProduit().AddProduit(5, 1, "Ordinateur APPLE", 1850);
+            //new RepoProduit().AddProduit(6, 2, "Biere STELLA", 5);
+            //new RepoProduit().AddProduit(7, 2, "Biere Heineken", 6);
+            //new RepoProduit().AddProduit(8, 3, "Pomme", 3);
+            //new RepoProduit().AddProduit(9, 3, "Raisin", 4);
+            //new RepoProduit().AddProduit(10, 4, "Pantalon", 7);
+            //new RepoProduit().AddProduit(11, 4, "Robe", 8);
+
+            var produits = new RepoProduit().ListProduits();
+
+            produits.ForEach(x => Console.WriteLine("Produit: " + x.Description + "  Categorie: " + x.IdCategorie + "  Prix: "+ x.Valeur));
+
+            Console.ReadKey();
 
         }
     }
---
+++ Linq/Linq02/Magasin.Infra.Linq/Repositorie/RepoProduit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magasin.Infra.Linq.Repositorie
+{
+    public class RepoProduit
+    {
+        public void AddProduit(int id, int idCategorie, string nom, decimal valeur)
+        {
+            Produit prod = new Produit();
+            prod.Id = id;
+            prod.IdCategorie = idCategorie;
+            prod.Description = nom;
+            prod.Valeur = valeur;
+
+            MagasinDataContext linq = new MagasinDataContext();
+            linq.Produit.InsertOnSubmit(prod);
+
+            linq.SubmitChanges();
+        }
+
+        public void ModifierProduit(int id, string nom)
+        {
+            MagasinDataContext linq = new MagasinDataContext();
+
+            var prod = linq.Produit.First(x => x.Id == id);
+
+            prod.Description = nom;
+
+
+            linq.SubmitChanges();
+        }
+
+
+        public void SupprimerProduit(int id)
+        {
+            MagasinDataContext linq = new MagasinDataContext();
+
+            var prod = linq.Produit.First(x => x.Id == id);
+
+            linq.Produit.DeleteOnSubmit(prod);
+
+            linq.SubmitChanges();
+        }
+
+        public List<Produit> ListProduits()
+        {
+            MagasinDataContext linq = new MagasinDataContext();
+
+            return linq.Produit.ToList();
+
+        }
+            
+
+
+    }
+}
+
